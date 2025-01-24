5a72efef51bf23b2fdc27b6b5b2242128ca85f6e
lingnath <32849838+lingnath@users.noreply.github.com>
2025-01-24 10:12:34 -0800

    Update airflow_bash_script.sh

--- airflow_setup/airflow_bash_script.sh
+++ airflow_setup/airflow_bash_script.sh
@@ -4,12 +4,14 @@ sudo apt-get update
 sudo apt-get install python3.12 -y
 sudo apt-get install pip -y
 git clone https://github.com/apache/airflow.git
+cd airflow && touch .env
 sudo apt install python3.12-venv
 python3 -m venv airflow_env
 source airflow_env/bin/activate
 pip install apache-airflow
 pip install apache-airflow[celery]
 pip install apache-airflow[postgres]
+pip install python-dotenv
 airflow db init
 airflow users create \
     --username admin \
