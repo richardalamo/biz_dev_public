d8c3907d6a062d3d8521456328cf34b428893ea5
lingnath <32849838+lingnath@users.noreply.github.com>
2025-01-24 09:51:23 -0800

    Update airflow_bash_script.sh

--- airflow_setup/airflow_bash_script.sh
+++ airflow_setup/airflow_bash_script.sh
@@ -6,3 +6,18 @@ sudo apt-get install pip -y
 git clone https://github.com/apache/airflow.git
 sudo apt install python3.12-venv
 python3 -m venv airflow_env
+source airflow_env/bin/activate
+pip install apache-airflow
+pip install apache-airflow[celery]
+pip install apache-airflow[postgres]
+airflow db init
+airflow users create \
+    --username admin \
+    --firstname Admin \
+    --lastname User \
+    --email admin@example.com \
+    --role Admin \
+    --password admin
+nohup airflow webserver --port 8080 &
+nohup airflow scheduler &
+mkdir ~/airflow/dags
