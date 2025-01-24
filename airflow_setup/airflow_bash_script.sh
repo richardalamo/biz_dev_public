d1f5f151e9b04de8df67883ae8d3a981255bfb3c
lingnath <32849838+lingnath@users.noreply.github.com>
2025-01-24 09:15:59 -0800

    Update airflow_bash_script.sh

--- airflow_setup/airflow_bash_script.sh
+++ airflow_setup/airflow_bash_script.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+sudo apt-get update
+sudo apt-get install python3.12 -y
+sudo apt-get install pip -y
+git clone https://github.com/apache/airflow.git
+sudo apt install python3.12-venv
+python3 -m venv airflow_env
