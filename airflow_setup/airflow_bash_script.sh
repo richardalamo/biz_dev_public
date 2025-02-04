814d0f433b8f99d136c135f3879bc3059ffea9c8
lingnath <32849838+lingnath@users.noreply.github.com>
2025-02-04 10:45:40 -0800

    Update airflow_bash_script.sh

--- airflow_setup/airflow_bash_script.sh
+++ airflow_setup/airflow_bash_script.sh
@@ -15,6 +15,7 @@ git clone https://github.com/apache/airflow.git
 # Set up airflow environment and virtual environment
 cd airflow && touch .env
 mkdir ~/airflow/dags
+mkdir ~/airflow/outputs
 sudo apt install python3.12-venv
 python3 -m venv airflow_env
 source airflow_env/bin/activate
