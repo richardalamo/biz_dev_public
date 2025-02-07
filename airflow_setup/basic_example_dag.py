5017603e37122bd61a806e22acba33f12c642d99
lingnath <32849838+lingnath@users.noreply.github.com>
2025-02-07 11:04:48 -0800

    Update basic_example_dag.py

--- airflow_setup/basic_example_dag.py
+++ airflow_setup/basic_example_dag.py
@@ -14,12 +14,14 @@ import json
 load_dotenv("/home/ubuntu/airflow/.env")
 airflow_project_path = "/home/ubuntu/airflow"
 output_path = "outputs"
-output_file = "jobs_detail_Data_Analyst_2024-05-09.csv"
+output_file = "outputs.csv"
 s3_bucket = "indeed-scrape"
 s3_folder = 'raw_scrapes/test'
 POSTGRES_CONN_ID = "airflow_rds"
-CSV_FILE_PATH = "/home/ubuntu/airflow/outputs/jobs_detail_Data_Analyst_2024-05-09.csv"
-
+CSV_FILE_PATH = "/home/ubuntu/airflow/outputs/outputs.csv"
+scrape_script_listings_path = '/home/ubuntu/airflow/scrape_code/1-Indeed_job_listings_ScraperAPI_Saudi.py'
+scrape_script_details_path = '/home/ubuntu/airflow/scrape_code/2-Indeed_job_details_ScraperAPI_Saudi.py'
+csv_aggregator_path = '/home/ubuntu/airflow/scrape_code/3-csv_aggregator_processor.py'
 
 to_del_file = os.getenv("to_del_folder")
 access_key = os.getenv("aws_access_key")
@@ -29,22 +31,6 @@ s3_client = boto3.client("s3",
                             aws_access_key_id=access_key, 
                             aws_secret_access_key=secret_access_key
 )
-
-def invoke_lambda():
-
-    lambda_client = boto3.client("lambda", 
-                                region_name='ca-central-1',
-                                aws_access_key_id=access_key, 
-                                aws_secret_access_key=secret_access_key
-    )
-    payload = {'action': 'stop'}
-    response = lambda_client.invoke(
-        FunctionName='start_ec2_function',
-        InvocationType='RequestResponse',
-        Payload=json.dumps(payload),
-    )
-    print(response['Payload'].read().decode('utf-8'))
-
 
 # Define the default arguments
 default_args = {
@@ -89,20 +75,20 @@ with DAG(
         task_id='start',
     )
 
-    # sleep = BashOperator(
-    #     task_id='sleep',
-    #     bash_command='sleep 300',
-    # )
-    
-    # add_to_log = BashOperator(
-    #     task_id='add_to_log',
-    #     bash_command=f'cd /home/ubuntu && echo "airflow turtle $(date)" >> {to_del_file}',
-    # )
+    scrape_listings = BashOperator(
+        task_id='scrape_listings',
+        bash_command=f'nohup python3 {scrape_script_listings_path} &'
+    )
 
-    # stop_ec2_task = PythonOperator(
-    #     task_id='invoke_lambda_to_stop_ec2',
-    #     python_callable=invoke_lambda,
-    # )
+    scrape_details = BashOperator(
+        task_id='scrape_details',
+        bash_command=f'nohup python3 {scrape_script_details_path} &'
+    )
+
+    csv_aggregator = BashOperator(
+        task_id='csv_aggregator',
+        bash_command=f'nohup python3 {csv_aggregator_path} &'
+    )
 
     task_upload_to_s3 = PythonOperator(
         task_id='Upload_to_S3',
@@ -127,7 +113,5 @@ with DAG(
     )
     
     # Set task dependencies
-    start_task >> task_upload_to_s3 >> load_csv_task >> end_task
-    # start_task >> add_to_log >> task_upload_to_s3 >> end_task
-    # start_task >> add_to_log >> sleep >> end_task
-    # start_task >> add_to_log >> stop_ec2_task
+    start_task >> scrape_listings >> scrape_details >> csv_aggregator >> task_upload_to_s3 >> load_csv_task >> end_task
+    # start_task >> load_csv_task >> end_task
