820635c1c90aa277dcf4f668855b7d36301f6aaf
Nathan Ling <32849838+lingnath@users.noreply.github.com>
2025-03-29 15:27:10 -0700

    Delete airflow_setup/ARCHIVED_per_boot_run.sh

--- airflow_setup/ARCHIVED_per_boot_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Copy automate_airflow.sh contents to the per-boot directory so that this script runs every time we boot up the ec2
-sudo sh -c 'cat automate_airflow.sh > /var/lib/cloud/scripts/per-boot/automated-script.sh'
-
-# Make the script executable
-sudo chmod +x /var/lib/cloud/scripts/per-boot/automated-script.sh
