f203aa2307c4d7d3d484417a7b65abf767f3f95b
Nathan Ling <32849838+lingnath@users.noreply.github.com>
2025-06-16 13:27:21 -0700

    Update indeed_etl_us.py

--- data_engineering/indeed_etl_us.py
+++ data_engineering/indeed_etl_us.py
@@ -13,23 +13,23 @@ import pytz
 import pandas as pd
 
 dag_name = 'indeed_etl_us'
-load_dotenv("/home/ubuntu/airflow/.env")
+env_path = '/home/ubuntu/airflow/.env'
+load_dotenv(env_path)
 scrape_code_path = '/home/ubuntu/airflow/scrape_code'
 indeed_api_path = f'{scrape_code_path}/concurrent_bright_data_scraper.py'
 concatenation_path = f'{scrape_code_path}/file_concatenation.py'
-s3_bucket = "indeed-scrape"
-s3_folder_source = 'bright_data/'
-config_path = '/home/ubuntu/airflow/scrape_code/bright_data_config/config.yaml'
+config_path = '/home/ubuntu/airflow/scrape_code/config.yaml'
 location = 'United_States'
 log_location = '/home/ubuntu/airflow/scrape_code/logs'
 location_index = 2 # 0 for Saudi. 1 for Canada. 2 for USA
-env_path = '/home/ubuntu/airflow/scrape_code/bright_data_config/.env'
 csv_file_path_ca_us = '/home/ubuntu/airflow/outputs/concatenated_data_us.csv'
 eastern = pytz.timezone('America/New_York')
 today_date = datetime.now(eastern).strftime('%Y-%m-%d')
 
 access_key = os.getenv("aws_access_key")
 secret_access_key = os.getenv("aws_secret_access_key")
+s3_bucket = os.getenv("S3_BUCKET")
+s3_folder_source = os.getenv("S3_DIRECTORY")
 sql_table = 'job_postings_us_ca'
 POSTGRES_CONN_ID = "airflow_rds"
 
@@ -96,14 +96,14 @@ wait_for_s3 = BashOperator(
     task_id='wait_for_s3',
     bash_command=f'sleep 60',
     dag=dag,
-    trigger_rule=TriggerRule.ALL_DONE,
+    # trigger_rule=TriggerRule.ALL_DONE,
 )
 
 concatenate_data = BashOperator(
     task_id='concatenate_data',
     bash_command=f'python3 {concatenation_path} --bucket {s3_bucket} --prefix {s3_folder_source} --output_csv_path {csv_file_paths[0]} --output_csv_path_ca_us {csv_file_path_ca_us} --access_key {access_key} --secret_access_key {secret_access_key} --location {location} --today_date {today_date}',
     dag=dag,
-    trigger_rule=TriggerRule.ALL_DONE,
+    # trigger_rule=TriggerRule.ALL_DONE,
     do_xcom_push=False,
 )
 
@@ -111,13 +111,13 @@ push_to_postgres = PythonOperator(
     task_id='push_csv_to_postgres',
     python_callable=load_csv_to_postgres,
     dag=dag,
-    trigger_rule=TriggerRule.ALL_DONE,
+    # trigger_rule=TriggerRule.ALL_DONE,
 )
 
 end_task = DummyOperator(
     task_id='end',
     dag=dag,
-    trigger_rule=TriggerRule.ALL_DONE,
+    # trigger_rule=TriggerRule.ALL_DONE,
 )
 
 start_task >> collect_jobs >> wait_for_s3 >> concatenate_data >> push_to_postgres >> end_task
