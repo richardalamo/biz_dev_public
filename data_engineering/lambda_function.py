9e8d2e4f8cd6c9a86a931121846405ce0a5f68c9
Nathan Ling <32849838+lingnath@users.noreply.github.com>
2025-06-17 17:24:25 -0700

    Update lambda_function.py

--- data_engineering/lambda_function.py
+++ data_engineering/lambda_function.py
@@ -17,41 +17,53 @@ locations_dict = {
 
 def start_instance(dag_name):
     """Starts EC2 instance. Executes a .sh script in EC2 command line."""
-    response = ec2.start_instances(InstanceIds=[INSTANCE_ID])
-    print(f"Starting instance {INSTANCE_ID}: {response}")
-    
-    # Wait for the instance to start
-    waiter = ec2.get_waiter('instance_running')
-    waiter.wait(InstanceIds=[INSTANCE_ID])
-    print(f"Instance {INSTANCE_ID} is now running.")
+    try:
+        response = ec2.start_instances(InstanceIds=[INSTANCE_ID])
+        print(f"Starting instance {INSTANCE_ID}: {response}")
+        
+        # Wait for the instance to start
+        waiter = ec2.get_waiter('instance_running')
+        waiter.wait(InstanceIds=[INSTANCE_ID])
+        print(f"Instance {INSTANCE_ID} is now running.")
 
-    print('Waiting for 30 seconds before we run ssm')
-    time.sleep(30)
+        print('Waiting for 30 seconds before we run ssm')
+        time.sleep(30)
+    except Exception as e:
+        print(f"Error with starting EC2 instance: {e}")
+
 
     # Execute the command to run ./automate_airflow.sh via SSM
-    response = ssm.send_command(
-        InstanceIds=[INSTANCE_ID],
-        DocumentName="AWS-RunShellScript",  # Use AWS-RunShellScript to run a shell command
-        Parameters={
-            'commands': [
-                'cd /home/ubuntu',
-                'chmod +x automate_airflow.sh',
-                'export AIRFLOW_HOME=/home/ubuntu/airflow',
-                f'sudo -u ubuntu ./automate_airflow.sh {dag_name}' # Starts Airflow, runs Airflow DAG, sends a Lambda call to stop ec2 instance
-            ]
-        }
-    )
-    print(f"Sent command to run {dag_name} DAG:", response)
+    try:
+        response = ssm.send_command(
+            InstanceIds=[INSTANCE_ID],
+            DocumentName="AWS-RunShellScript",  # Use AWS-RunShellScript to run a shell command
+            Parameters={
+                'commands': [
+                    'cd /home/ubuntu',
+                    'chmod +x automate_airflow.sh',
+                    'export AIRFLOW_HOME=/home/ubuntu/airflow',
+                    f'sudo -u ubuntu ./automate_airflow.sh {dag_name}' # Starts Airflow, runs Airflow DAG, sends a Lambda call to stop ec2 instance
+                ]
+            }
+        )
+        print(f"Sent command to run {dag_name} DAG:", response)
+    except Exception as e:
+        print(f"Error with executing SSM command: {e}")
+
 
 def stop_instance():
     """Stops EC2 instance."""
-    response = ec2.stop_instances(InstanceIds=[INSTANCE_ID])
-    print(f"Stopping instance {INSTANCE_ID}: {response}")
-    
-    # Wait for the instance to stop
-    waiter = ec2.get_waiter('instance_stopped')
-    waiter.wait(InstanceIds=[INSTANCE_ID])
-    print(f"Instance {INSTANCE_ID} is now stopped.")
+    try:
+        response = ec2.stop_instances(InstanceIds=[INSTANCE_ID])
+        print(f"Stopping instance {INSTANCE_ID}: {response}")
+        
+        # Wait for the instance to stop
+        waiter = ec2.get_waiter('instance_stopped')
+        waiter.wait(InstanceIds=[INSTANCE_ID])
+        print(f"Instance {INSTANCE_ID} is now stopped.")
+    except Exception as e:
+        print(f"Error with stopping EC2 instance: {e}")
+
 
 def lambda_handler(event, context):
     """Depending on the event, we either start or stop the EC2 instance."""
