d5503c511b7c1cbb17d4e4c5721821cd30ab0799
Nathan Ling <32849838+lingnath@users.noreply.github.com>
2025-06-19 08:38:50 -0700

    Update stored_functions.py

--- data_engineering/stored_functions.py
+++ data_engineering/stored_functions.py
@@ -5,20 +5,23 @@ from dotenv import load_dotenv
 
 load_dotenv("/home/ubuntu/airflow/.env")
 
-SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")  # or hardcode if needed
+SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")  # Get the webhook url. Which will connect Airflow to the Slack channel to notify users
 user_id = os.getenv("SLACK_ID")
 
+# Send message to specified Slack channel
 def send_slack_message(message):
     if SLACK_WEBHOOK_URL is None:
         raise ValueError("Slack webhook URL is not set")
     payload = {"text": message}
     requests.post(SLACK_WEBHOOK_URL, json=payload)
 
+# If Airflow task succeeds, a certain message will be sent to a specified Slack channel
 def task_success_callback(context):
     task = context.get("task_instance").task_id
     dag = context.get("dag").dag_id
     send_slack_message(f"✅ Task *{task}* in DAG *{dag}* succeeded.")
 
+# If Airflow task succeeds, another message format will be sent to a specified Slack channel
 def task_failure_callback(context):
     task = context.get("task_instance").task_id
     dag = context.get("dag").dag_id
