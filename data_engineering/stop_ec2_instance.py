e17932e86b76c23ef2ba30c853b17a6ddfbbd2d5
Nathan Ling <32849838+lingnath@users.noreply.github.com>
2025-07-17 09:54:20 -0700

    Update stop_ec2_instance.py

--- data_engineering/stop_ec2_instance.py
+++ data_engineering/stop_ec2_instance.py
@@ -10,7 +10,7 @@ secret_access_key = os.getenv("aws_secret_access_key")
 
 # Create Lambda client
 lambda_client = boto3.client("lambda", 
-                                region_name='ca-central-1',
+                                region_name='us-east-1',
                                 aws_access_key_id=access_key, 
                                 aws_secret_access_key=secret_access_key
                                 )
@@ -18,7 +18,7 @@ lambda_client = boto3.client("lambda",
 # Send payload to AWS Lambda function to stop EC2 instance
 payload = {'action': 'stop'}
 response = lambda_client.invoke(
-    FunctionName='start_ec2_function',
+    FunctionName='start_stop_job_indeed_scrapes',
     InvocationType='RequestResponse',
     Payload=json.dumps(payload),
 )
